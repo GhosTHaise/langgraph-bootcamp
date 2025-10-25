aebb0385cdb1a472ec3562aa31a8aaefe872b7d5
GhosTHaise <ghostrex2@gmail.com>
2025-10-25 19:15:16 +0300

    feat : init llm

--- notebook/agents/1_Agent_Bot.py
+++ notebook/agents/1_Agent_Bot.py
@@ -3,3 +3,11 @@ from langchain_core.messages import HumanMessage
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+    
+llm = ChatGroq(model="llama-3.1-8b-instant")
