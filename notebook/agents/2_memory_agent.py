9cb4c5c667bd0c47ea56148b1b481ea6f58d656c
GhosTHaise <ghostrex2@gmail.com>
2025-10-28 20:57:55 +0300

    feat : conversation_history

--- notebook/agents/2_memory_agent.py
+++ notebook/agents/2_memory_agent.py
@@ -1,5 +1,5 @@
 import os
-from typing import TypedDict, List
+from typing import TypedDict, List, Union
 from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv
@@ -7,3 +7,36 @@ from langchain_groq import ChatGroq
 
 
 load_dotenv()
+
+class AgentState(TypedDict):
+    messages : List[Union[HumanMessage, AIMessage]]
+    
+llm = ChatGroq(model="llama-3.1-8b-instant")
+
+def process(state : AgentState) -> AgentState:
+    """This node will do solve the request you input."""
+    response = llm.invoke(state["messages"])
+    
+    state["messages"].append(AIMessage(content=response.content))
+    print(f"\nAI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+
+agent = graph.compile()
+
+conversation_history = []
+
+user_input = input("Enter : ")
+while user_input != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages" : conversation_history})
+    
+    print(result["messages"])
+    conversation_history = result["messages"]
+    
+    user_input = input("Enter : ")
