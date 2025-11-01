6ef23de0b4129384f05316c52615a5b5c0fe2668
GhosTHaise <ghostrex2@gmail.com>
2025-11-01 20:08:19 +0300

    feat : model_call

--- notebook/agents/3_react_agent.py
+++ notebook/agents/3_react_agent.py
@@ -31,3 +31,38 @@ print(email.__metadata__)
 | `add_messages`               | Ajoute des messages à un état | `add_messages(history, [msg])`     |
 | `ToolNode`                   | Nœud prêt pour les outils     | `ToolNode(tools=[my_tool])`        |
  """
+ 
+def decorateur(func):
+    def wrapper():
+        print("Avant la fonction")
+        func()  # exécute la fonction originale
+        print("Après la fonction")
+    return wrapper
+
+@decorateur
+def dire_bonjour():
+    print("Bonjour !")
+
+dire_bonjour()
+
+
+class AgentState(TypedDict):
+    messages : Annotated[Sequence[BaseMessage], add_messages]
+    
+@tool
+def add(a : int , b : int):
+    """ This is an addition function that adds two numbers together """
+    
+    return a + b
+
+tools = [add]
+
+model = ChatGroq(model="llama-3.1-8b-instant").bind_tools(tools)
+
+
+def model_call(state : AgentState) -> AgentState:
+    system_prompt = SystemMessage(content="You are my AI assistant, please answer my query to the best of your ability.")
+    response = model.invoke([system_prompt] + state["messages"])
+    
+    return {"messages" : [response]}  
+
