6cd0b4839799ef861a10316df1b4fd460e9a4107
GhosTHaise <ghostrex2@gmail.com>
2025-11-05 20:33:29 +0300

    feat : create vectore store

--- notebook/agents/5_rag.py
+++ notebook/agents/5_rag.py
@@ -18,7 +18,7 @@ load_dotenv()
 llm = ChatGroq(model="llama-3.1-8b-instant",temperature=0)
 
 # Our Embedding Model - has to also be compatible with the LLM -> gemini-embedding-001
-embeddings = GoogleGenerativeAIEmbeddings(model="text-embedding-ada-002")
+embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
 
 pdf_path = "notebook/agents/pdf/GeForce-RTX-5070-12G-GAMING-TRIO-OC.pdf"
 
@@ -32,4 +32,32 @@ try:
     print(f"PDF Loaded with {len(pages)} pages")
 except Exception as e:
     print(f"Error loading PDF: {e}")
+    raise e
+
+# Chunking Process
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+
+pages_split = text_splitter.split_documents(pages)
+
+persist_directory = "notebook/agents/db"
+collection_name = "stock_market"
+
+# create the folder if not exist
+if not os.path.exists(persist_directory):
+    os.makedirs(persist_directory)
+
+try:
+    vectorstore = Chroma.from_documents(
+        documents=pages_split,
+        embedding=embeddings,
+        persist_directory=persist_directory,
+        collection_name=collection_name,
+    )
+    print(f"Chroma DB created at {persist_directory}")
+    
+except Exception as e:
+    print(f"Error creating Chroma DB: {e}")
     raise e
